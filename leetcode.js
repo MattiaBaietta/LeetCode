77df365bb9cd34c662387c69bba389948f74ef2b
MBaietta <mattia.baietta89@gmail.com>
2025-05-12 23:38:28 +0200

    problem 2 solved

--- leetcode.js
+++ leetcode.js
@@ -1,3 +1,6 @@
+// Problem 1
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
 function hasDuplicate(nums) {
   const map1 = new Map();
   for (let i = 0; i < nums.length; i++) {
@@ -12,3 +15,15 @@ function hasDuplicate(nums) {
 
 hasDuplicate([1, 2, 3, 3]);
 hasDuplicate([1, 2, 3, 4]);
+
+// Problem 2
+// Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+function isAnagram(s, t) {
+  const splitted1 = s.split("").sort().join("");
+  const splitted2 = t.split("").sort().join("");
+  return splitted1 == splitted2 ? true : false;
+}
+
+isAnagram("racecar", "carrace");
+isAnagram("jar", "jam");
